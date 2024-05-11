9c32463ea893949d3ed4c4a14788e0185067d025
Francisco alfaro <franciscoalfar@gmail.com>
2024-05-11 16:06:19 -0400

    mejora svg

--- src/components/publication/Checkout.jsx
+++ src/components/publication/Checkout.jsx
@@ -66,7 +66,7 @@ export const Checkout = () => {
       comuna: selectedCommune,
       region: selectedRegion,
       products: cart.map(item => ({
-        size:item.size,
+        size: item.size,
         product: item._id,
         quantity: item.quantity,
         priceunitary: item.offerprice && parseFloat(item.offerprice) > 0 ? item.offerprice : item.price
@@ -99,7 +99,7 @@ export const Checkout = () => {
             setNewOrden(data)
           }
         });
-      } else if(data.status === 'error') {
+      } else if (data.status === 'error') {
         Swal.fire({
           position: 'center',
           icon: 'warning',
@@ -126,7 +126,12 @@ export const Checkout = () => {
     <div className="container">
       <main>
         <div className="py-5 text-center">
-          <img className="d-block mx-auto mb-4" src="../../../src/assets/img/cart2.svg" alt="" width="72" height="57" />
+          <img className="d-block mx-auto mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="72" height="57" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+              <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+            </svg>
+          </img>
+
           <h2>Confirmar Compra</h2>
         </div>
 
